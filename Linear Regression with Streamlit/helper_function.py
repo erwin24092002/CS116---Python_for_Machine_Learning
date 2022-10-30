a0165c8a8c3db12a143909436ebbfb628a87fb9f
Trương Thành Thắng <20521907@gm.uit.edu.vn>
2022-10-30 20:33:28 +0700

    update lots

--- Linear Regression with Streamlit/helper_function.py
+++ Linear Regression with Streamlit/helper_function.py
@@ -7,10 +7,11 @@ def addlabels(x, y, color):
         plt.text(i, y[i], y[i], ha = 'center', fontsize=10, bbox = dict(facecolor = color, alpha =.6))
 
 def plot_bar(x_data, y_data, color, x_label, y_label, title):
-    plt.bar(x_data, y_data, color = color, width = 0.4)
+    plt.bar(x_data, y_data, color = color, width = 0.5)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.xticks(fontsize=10)
     plt.yticks(fontsize=10)
+    plt.ylim((0, max(y_data)*1.2))
     addlabels(x_data, y_data, color)
     plt.title(title)
--- Linear Regression with Streamlit/main.py
+++ Linear Regression with Streamlit/main.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 from helper_function import *
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 
@@ -39,20 +40,17 @@ if uploaded_file is not None:
         cbox = cols[int(i/len(features)*4)].checkbox(features[i])
         if cbox:
             input_features.append(features[i])
+
+    # CHOSE TYPE OF SPLITTING DATA
+    st.header("Type of Splitting Data")
+    split_type = st.selectbox(" ", ("Train-Test Split", "K-Fold Cross Validation"), label_visibility="collapsed")
     cols = st.columns(2)
     with cols[0]:
         st.header("Output Feature")
         output_feature = st.selectbox(" ", 
             label_visibility='collapsed',
             options = [feature for feature in features if feature not in input_features and df.dtypes[feature] != 'object'])
-        
-    # SET RATIO TO SPLIT DATA
-    with cols[1]:
-        st.header("Train Ratio")
-        data_ratio = st.slider(label='Select a range of values', 
-            label_visibility='collapsed', 
-            min_value=0.0, max_value=1.0, value=0.5, step=0.01)
-
+    
     # PREPROCESS DATA
     encs = []
     Y = df[output_feature].to_numpy()
@@ -68,29 +66,75 @@ if uploaded_file is not None:
             X = x
         else:
             X = np.concatenate((X, x), axis=1)
-    
+
+    # SET RATIO TO SPLIT DATA
+    with cols[1]:
+        if split_type == "Train-Test Split":
+            st.header("Train Ratio")
+            data_ratio = st.slider(label='Select a range of values', 
+                label_visibility='collapsed', 
+                min_value=0.0, max_value=1.0, value=0.5, step=0.01)
+        else: 
+            st.header("Numbers of Fold")
+            k_fold = st.selectbox(" ", range(2, X.shape[0]), label_visibility="collapsed")
+
     # TRAIN MODEL
     st.header("Train Model")    
     cols = st.columns(11)
     with cols[5]: 
         btn_run = st.button("Run")
-    if btn_run:
+    if btn_run and split_type=="Train-Test Split":
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=data_ratio, random_state=1907)
         model = LinearRegression().fit(X_train, Y_train)
         Y_pred = model.predict(X_test)
         mae = mean_absolute_error(y_true=Y_test, y_pred=Y_pred)
         mse = mean_squared_error(y_true=Y_test, y_pred=Y_pred)
         plt.figure(figsize=(8, 4))
-        plot_bar(np.array(['Mean Absolute Error', 'Mean Squered Error']), 
-            np.array([round(mae,2), round(mse,2)]), 
-            color='maroon', x_label="", y_label="", 
-            title= "EVALUATION METRICS")
-        plt.savefig('chart.png')
+        plot_bar(np.array([str(data_ratio)]), 
+            np.array([round(mae,2)]), 
+            color='maroon', x_label="Train Data Ratio", y_label="Loss", 
+            title= "MEAN ABSOLUTE ERROR")
+        plt.savefig('mae.png')
+        plt.figure(figsize=(8, 4))
+        plot_bar(np.array([str(data_ratio)]), 
+            np.array([round(mse,2)]), 
+            color='green', x_label="Train Data Ratio", y_label="Loss", 
+            title= "MEAN SQUARED ERROR")
+        plt.savefig('mse.png')
         with open('model.pkl','wb') as f:
             pickle.dump(model, f)
-    img = cv2.imread('chart.png')
-    if img is not None: 
-        st.image(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))    
+    elif btn_run: 
+        kf = KFold(n_splits=k_fold, random_state=None)
+        folds = [str(fold) for fold in range(1, k_fold+1)]
+        mae = []
+        mse = []
+        for train_index, test_index in kf.split(X):
+            X_train, X_test = X[train_index, :], X[test_index, :]
+            Y_train, Y_test = Y[train_index], Y[test_index]
+            model = LinearRegression().fit(X_train, Y_train)
+            Y_pred = model.predict(X_test)
+            mae.append(round(mean_absolute_error(y_true=Y_test, y_pred=Y_pred), 2))
+            mse.append(round(mean_squared_error(y_true=Y_test, y_pred=Y_pred), 2))
+            with open('model.pkl','wb') as f:
+                pickle.dump(model, f)
+        plt.figure(figsize=(8, 4))
+        plot_bar(np.array(folds), 
+            np.array(mae), 
+            color='maroon', x_label="Folds", y_label="Loss", 
+            title= "MEAN ABSOLUTE ERROR")
+        plt.savefig('mae.png')
+        plt.figure(figsize=(8, 4))
+        plot_bar(np.array(folds), 
+            np.array(mse), 
+            color='green', x_label="Folds", y_label="Loss", 
+            title= "MEAN SQUARED ERROR")
+        plt.savefig('mse.png')
+    mae_img = cv2.imread('mae.png')
+    if mae_img is not None: 
+        st.image(cv2.cvtColor(mae_img, cv2.COLOR_BGR2RGB)) 
+    mse_img = cv2.imread('mse.png')
+    if mse_img is not None: 
+        st.image(cv2.cvtColor(mse_img, cv2.COLOR_BGR2RGB))      
 
     # SET INPUT
     st.header("Test Model")    

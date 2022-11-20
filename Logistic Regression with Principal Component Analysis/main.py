6da2be6cdf299c494d9db988c58ab7ce7cae5301
Trương Thành Thắng <20521907@gm.uit.edu.vn>
2022-11-20 09:51:31 +0700

    update lots

--- Logistic Regression with Principal Component Analysis/main.py
+++ Logistic Regression with Principal Component Analysis/main.py
@@ -1,6 +1,9 @@
 import streamlit as st
-
-
+from sklearn.datasets import load_wine
+import numpy as np
+import pandas as pd
+import cv2
+import matplotlib.pyplot as plt
 
 st.title("LOGISTIC REGRESSION WITH PRINCIPAL COMPONENT ANALYSIS")
 st.markdown(
@@ -9,6 +12,25 @@ st.markdown(
     ### MSSV: 20521907
     """
 )
+
+print("-----------------------------REFRESHED-------------------------")
+# wine image header
+wine_img = cv2.imread('wine_image.jpg')
+wine_img = cv2.cvtColor(wine_img, cv2.COLOR_BGR2RGB)
+st.image(wine_img, width=700)
+
+# load wine dataset
+wine = load_wine()
+
+# display 5 head datapoint
+st.header("WINE DATASET")
+wine_df = pd.DataFrame(wine["data"])
+wine_df.columns = wine["feature_names"]
+st.write(wine_df.head(5))
+
+
+st.write(wine)
+
 
 
 
